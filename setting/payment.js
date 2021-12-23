01159534a011671e460771e99586dd538ad0c552
BOTZ-DHANI <dhanigans87@gmail.com>
2021-12-23 02:42:03 -0800

    SC V9

---
+++ setting/payment.js
@@ -0,0 +1,9 @@
+exports.payment = (prefix) => {
+return `╭──「 *PAYMENT / DONASI* 」
+│
+├❏ Gopay : _Tuh Kak Scan Qrnya_
+├❏ Dana : _Tidak Tersedia_
+├❏ Ovo : _Tidak Tersedia_
+│
+╰──`
+}
---
+++ setting/sewabot.js
@@ -0,0 +1,15 @@
+exports.sewabot = (prefix) => {
+return `╭──「 *SEWABOT* 」
+│
+├❏ 1 MINGGU : 5.000 ( 5K )
+├❏ 1 BULAN : 10.000 ( 10K )
+├❏ PERMANEN : 15.000 ( 15K )
+│
+├──「 *PREMIUM* 」
+│
+├❏ 1 MINGGU : 10.000 ( 10K )
+├❏ 1 BULAN : 15.000 ( 15K )
+├❏ PERMANEN : 20.000 ( 20K )
+│
+╰──「 *${botname}* 」`
+}
